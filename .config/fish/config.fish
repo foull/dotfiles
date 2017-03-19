9e997ad8b1a93685074e3dc6bbdb1aee05e663bd
Krzysztof Lenda <fou@f17.pl>
2017-03-19 01:54:58 +0100

    update some settings in fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+# settings
+
+# bobthefish theme settings
+
+#- set nicer nerd fonts
+set -g theme_nerd_fonts yes
+#- show the full path (directory names)
+set -g fish_prompt_pwd_dir_length 0
+
+
+
+
+
+
+
+# source ~/dotfiles/.config/zsh/aliases.sh
+
+# aliases
+alias g="git"
